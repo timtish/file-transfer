442d31e9085c1b6f2a5cc9eb3e7c5b2c42e09672
Timofey Tishin <timtish@gmail.com>
2013-11-19 14:34:04 +0400

    zip encoding selection

--- src/main/java/ru/timtish/bridge/pipeline/converter/Zip.java
+++ src/main/java/ru/timtish/bridge/pipeline/converter/Zip.java
@@ -1,7 +1,15 @@
 package ru.timtish.bridge.pipeline.converter;
 
+import ru.timtish.bridge.box.BoxDirectory;
+import ru.timtish.bridge.box.BoxEntity;
+import ru.timtish.bridge.box.BoxZipFile;
+import ru.timtish.bridge.pipeline.AbstractStream;
+import ru.timtish.bridge.pipeline.cache.CachedInMemoryInputStream;
+import ru.timtish.bridge.pipeline.cache.RepeatableInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -9,13 +17,6 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
-
-import ru.timtish.bridge.box.BoxDirectory;
-import ru.timtish.bridge.box.BoxEntity;
-import ru.timtish.bridge.box.BoxZipFile;
-import ru.timtish.bridge.pipeline.AbstractStream;
-import ru.timtish.bridge.pipeline.cache.CachedInMemoryInputStream;
-import ru.timtish.bridge.pipeline.cache.RepeatableInputStream;
 
 /**
  * @author Timofey Tishin (ttishin@luxoft.com)
@@ -95,7 +96,7 @@ public class Zip {
 	}
 
 	public static List<BoxEntity> getFilesTree(AbstractStream inputStream) throws IOException {
-		ZipInputStream zip = new ZipInputStream(inputStream.createCopy());
+		ZipInputStream zip = new ZipInputStream(inputStream.createCopy(), selectFileNamesEncoding(inputStream));
 		try {
 			return getChilds(zip, inputStream);
 		} finally {
@@ -103,8 +104,32 @@ public class Zip {
 		}
 	}
 
-	public static InputStream unzip(InputStream inputStream, String path) throws IOException {
-		ZipInputStream zip = new ZipInputStream(inputStream);
+  private static Charset selectFileNamesEncoding(AbstractStream inputStream) throws IOException {
+    if (testEncoding(inputStream, Charset.forName("UTF8"))) return Charset.forName("UTF8");
+    if (testEncoding(inputStream, Charset.forName("CP866"))) return Charset.forName("CP866");
+    if (testEncoding(inputStream, Charset.forName("CP1251"))) return Charset.forName("CP1251");
+    return Charset.defaultCharset();
+  }
+
+  private static boolean testEncoding(AbstractStream inputStream, Charset charset) throws IOException {
+    InputStream in = inputStream.createCopy();
+    try {
+      ZipInputStream zip = new ZipInputStream(in, charset);
+      while (zip.getNextEntry() != null) {}
+      return true;
+    } catch (IllegalArgumentException e) {
+      return false;
+    } finally {
+      try {
+        in.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static InputStream unzip(InputStream inputStream, String path) throws IOException {
+		ZipInputStream zip = new ZipInputStream(inputStream, Charset.forName("CP866"));
 		RepeatableInputStream fileStream = null;
 		try {
 			ZipEntry zipFile;
--- src/test/java/ru/timtish/bridge/ZipTest.java
+++ src/test/java/ru/timtish/bridge/ZipTest.java
@@ -1,13 +1,13 @@
 package ru.timtish.bridge;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
 import org.junit.Test;
 import ru.timtish.bridge.box.BoxEntity;
 import ru.timtish.bridge.pipeline.AbstractStream;
 import ru.timtish.bridge.pipeline.converter.Zip;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,9 +23,11 @@ public class ZipTest {
 
 			if (System.getProperty("sun.zip.encoding") == null) System.setProperty("sun.zip.encoding", "CP866");
 
-			List<BoxEntity> files = Zip.getFilesTree(new AbstractStream(zip, (Long) null, null, null, null));
-			assertEquals(files.size(), 2);
-			assertEquals(files.get(0).getChilds().size(), 1);
+      AbstractStream stream = new AbstractStream(zip, (Long) null, null, null, null);
+      stream.setRepeatable(true); // todo: test not repeatable - event for event driven logic
+			List<BoxEntity> files = Zip.getFilesTree(stream);
+      assertEquals(1, files.get(0).getChilds().size());
+      assertEquals(2, files.size());
 
 		} finally {
 			zip.close();
